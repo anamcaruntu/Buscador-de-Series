6cfccd50fbe3e3d949a09faec20a9d12d53b3c16
Ana Maria Caruntu <anam.caruntu@gmail.com>
2020-07-09 23:50:01 +0200

    estructuraHtml

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+"use strict";
+
+//creo el array donde voy a guardar las búsquedas en mi api
+
+let series = [];
+let favorites = [];
+let giveUserNameSeries;
+
+//variables
+
+//  FUNCIÓN API
+
+const inputElement = document.querySelector(".js-input-name");
+const searchButton = document.querySelector(".js-btn-search");
