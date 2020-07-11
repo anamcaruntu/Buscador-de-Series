6210db3e6f27616eb6455f5534fdb8c970d940d7
Ana Maria Caruntu <anam.caruntu@gmail.com>
2020-07-11 13:49:49 +0200

    cambios

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,29 @@
 "use strict";
 
 //creo el array donde voy a guardar las búsquedas en mi api
+let savedData = [];
+let favData = [];
+let imageDefault = "https://via.placeholder.com/210x295/ffffff/666666/?text=TV";
 
-let series = [];
-let favorites = [];
-let giveUserNameSeries;
+//Voy a buscar el ul del listado de pelis
+const movieList = document.querySelector(".movie__list");
+const favList = document.querySelector(".aside__list");
+//voy a por el input
+const input = document.querySelector(".container__form__input");
+//voy a por el img
+const img = document.querySelector(".movie__list__item__img");
 
-//variables
+getFavsFromLocalStorage();
 
-//  FUNCIÓN API
+//fetch
 
-const inputElement = document.querySelector(".js-input-name");
-const searchButton = document.querySelector(".js-btn-search");
+function getDataFromServer() {
+  fetch(`http://api.tvmaze.com/search/shows?q=${input.value}`)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      paintAllCards(data);
+      savedData = data;
+    });
+}
